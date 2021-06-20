151e86996e586474a545426672a6b64a2a029bcb
klaud <klaud@DESKTOP-KQNJ5P2>
2021-06-20 12:55:34 +0200

    Menu+operations

---
+++ Triangle/Triangle/Operations.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+
+namespace Calculator
+{
+    public class Operations
+    {
+        public Operations(double num1, double num2)
+        {
+            Num1 = num1;
+            Num2 = num2;
+        }
+        public Operations(double num1)
+        {
+            Num1 = num1;
+        }
+
+        public double Num1 { get; set; }
+        public double Num2 { get; set; }
+
+        public double AddNumbers()
+        {
+            return Num1 + Num2;
+        }
+        public double SubtractNumbers()
+        {
+            return Num1 - Num2;
+        }
+        public double MultiplicateNumbers()
+        {
+            return Num1 * Num2;
+        }
+        public double DivideNumbers()
+        {
+            double divide;
+
+
+            while (Num2 == 0)
+            {
+                Console.WriteLine("Nie dzielimy przez 0! Proszę spróbować ponownie");
+                break;
+            }
+            divide = Num1 / Num2;
+
+
+            return divide;
+
+        }
+        public string IsNumberEven()
+        {
+            string even;
+            if (Num1 % 2 == 0)
+            {
+                even = "Podana liczba jest parzysta";
+            } else even = "Podana liczba jest parzysta";
+            return even;
+        }
+    }
+}
--- Triangle/Triangle/Program.cs
+++ Triangle/Triangle/Program.cs
@@ -1,12 +1,91 @@
 ﻿using System;
+using Calculator;
 
-namespace Triangle
+
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+            string again = "T";
+            while (again == "T")
+            {
+                PrintMenu();
+                Calc();
+                Console.WriteLine("Wykonać ponownie?(T/N)");
+                again = Console.ReadLine().ToUpper();
+                Console.Clear();
+            }
+            
         }
+
+        public static void PrintMenu()
+        {
+            Console.WriteLine("Witaj w kalkulatorze");
+            Console.WriteLine("Co chesz zrobić?");
+            Console.WriteLine("\t1 - Wykonać dodawanie");
+            Console.WriteLine("\t2 - Wykonać odejmowanie");
+            Console.WriteLine("\t3 - Wykonać mnożenie");
+            Console.WriteLine("\t4 - Wykonać dzielenie");
+            Console.WriteLine("\t5 - Sprawdzić parzystość");
+
+        }
+
+        public static void Calc()
+        {
+            Console.WriteLine("Klawisz menu");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            if (choice<=4)
+            {
+                Console.WriteLine("Podaj pierwszą liczbę");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Console.WriteLine("Podaj drugą");
+                double num2 = Convert.ToDouble(Console.ReadLine());
+                Operations operations = new Operations(num1, num2);
+                string printScore = "Wynik działania to: ";
+                switch (choice)
+                {
+                    case 1:
+                        Console.WriteLine(printScore + operations.AddNumbers());
+                        break;
+                    case 2:
+                        Console.WriteLine(printScore + operations.SubtractNumbers());
+                        break;
+                    case 3:
+                        Console.WriteLine(printScore + operations.MultiplicateNumbers());
+                        break;
+                    case 4:
+                        Console.WriteLine(printScore + operations.DivideNumbers());
+                        break;
+                    default:
+                        break;
+                }
+            }
+            else if (choice == 5)
+            {
+                Console.WriteLine("Podaj liczbę którą chcesz sprawdzić");
+                double num1 = Convert.ToDouble(Console.ReadLine());
+                Operations operations = new Operations(num1);
+                Console.WriteLine(operations.IsNumberEven());
+            }
+            else
+            {
+                Console.WriteLine("Wciśnięto błędny klawisz, proszę spróbować ponownie");
+                Calc();
+            }
+
+            }
+
+
+        }
+
+
+
     }
-}
+    
+
+
+
+
